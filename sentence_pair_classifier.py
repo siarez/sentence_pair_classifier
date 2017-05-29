b96a97055476a3b45d9ffc0a5417d5ac4da0af2c
siavash <sia@siarez.com>
2017-05-29 12:11:10 -0700

    Switched to euclidean distance, added time measurement

--- sentence_pair_classifier.py
+++ sentence_pair_classifier.py
@@ -1,12 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import time
 
 from tensorflow.contrib import learn
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-
+#a, b are sentence word embeddings
 a = tf.placeholder(dtype='float', shape=[None, 300], name='ai')
 b = tf.placeholder(dtype='float', shape=[None, 300], name='bi')
 label = tf.placeholder(dtype='float', shape=[1], name='label')
@@ -47,20 +48,21 @@ v2 = tf.reduce_sum(v2j, axis=0)
 v1_v2 = tf.concat([v1, v2], 0)
 v1_v2_1 = tf.reshape(v1_v2, [1, 400])
 
-y = tf.layers.dense(inputs=v1_v2_1, units=2, activation=tf.nn.sigmoid, use_bias=True,
+y = tf.layers.dense(inputs=v1_v2_1, units=1, activation=tf.nn.sigmoid, use_bias=True,
                 kernel_initializer=None, bias_initializer=None,
                      name='H')
 
-onehot_labels = tf.one_hot(indices=tf.cast(label, tf.int32), depth=2)
-loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=y)
+#onehot_labels = tf.one_hot(indices=tf.cast(label, tf.int32), depth=1)
+onehot_labels = tf.reshape(label, [1, 1])
+#loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=y)
+
+loss = tf.square(tf.reduce_sum(y - onehot_labels))
 
 train_op = tf.contrib.layers.optimize_loss(
     loss=loss,
     global_step=tf.contrib.framework.get_global_step(),
     learning_rate=0.01,
     optimizer="SGD")
-
-
 
 
 init_op = tf.global_variables_initializer()
@@ -71,9 +73,13 @@ with tf.Session() as sess:
 
     a_feed = np.random.rand(3, 300)
     b_feed = np.random.rand(5, 300)
-    label_feed = np.array([1])
+    label_feed = np.array([0])
 
-    for i in range(1000):
-        y1, losss, y2, one_hots = sess.run([train_op, loss, y, onehot_labels], {a: a_feed, b: b_feed, label: label_feed})
+    start_time = time.time()
+
+
+    for i in range(10000):
+        train_op1, losss, y2, one_hots = sess.run([train_op, loss, y, onehot_labels], {a: a_feed, b: b_feed, label: label_feed})
         if i % 100 == 0:
-            print(losss, y2, one_hots)
+            print(losss, y2, one_hots, 'sps: ', 100/(time.time() - start_time))
+            start_time = time.time()
